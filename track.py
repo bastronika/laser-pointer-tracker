7ed61f45d6bfa560be0eef5b4cca42c012cb82b1
slamet basori <basori.teub@gmail.com>
2020-06-02 07:46:56 +0700

    update track py to windows

--- track.py
+++ track.py
@@ -1,10 +1,14 @@
-#import win32api, win32con
-import time
-import pyautogui
+import win32api, win32con, win32gui
+#import pyautogui
 
+def tracking():
+    flags, hcursor, (x,y) = win32gui.GetCursorInfo()
+    print(x,y)
+    
 try:
     while True:
-        print pyautogui.position()
+        #print pyautogui.position()
+        tracking()
 
 except KeyboardInterrupt:
     print('\n')
