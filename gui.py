a941895fc946c1cb4f5878bb8e7ab0c5689f78fd
unknown <basori@MSBKBSHOW.id.longway.com>
2020-06-04 08:28:20 +0700

    click win32 function

--- gui.py
+++ gui.py
@@ -116,6 +116,12 @@ class ColorDetector(QDialog):
 
         self.h = None
 
+    def click(x,y):
+        win32api.SetCursorPos((x,y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)    
+        
+
     def push_reset(self):
 
         self.ltx_adj = 0
